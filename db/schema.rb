4aeb5c2d44cedf96e234f3277dc67dc6eabc8313
Aleksa <aleksa01miseljic@gmail.com>
2019-03-31 23:33:02 +0200

    Move tests to globally shared examples. Add logout and authorize

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_144630) do
+ActiveRecord::Schema.define(version: 2019_03_31_133508) do
+
+  create_table "access_tokens", force: :cascade do |t|
+    t.string "token", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_access_tokens_on_user_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login"
+    t.string "name"
+    t.string "url"
+    t.string "avatar_ur"
+    t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
